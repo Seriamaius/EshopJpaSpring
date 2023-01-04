74b621e9ad8cabb6dceb88c9caf6710a98b87a49
samuelstefancic <SAMUELSTEFANCIC@GMAIL.com>
2023-01-04 15:30:44 +0100

    Ajout des premiers éléments fournisseurs Que ça marche svp

--- src/main/java/eshop/service/FournisseurService.java
+++ src/main/java/eshop/service/FournisseurService.java
@@ -1,6 +1,7 @@
 package eshop.service;
 
 import eshop.entity.Fournisseur;
+import eshop.exception.FournisseurException;
 import eshop.repository.FournisseurRepository;
 import eshop.repository.ProduitRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,13 +17,24 @@ public class FournisseurService {
     private ProduitRepository produitRepository;
 
     public void create(Fournisseur fournisseur) {
-
+        checkFournisseurIsNotNull(fournisseur);
+        if (fournisseur.getId() == null) {
+            throw new FournisseurException("id is null");
+        }
+        if (fournisseur.getNom() == null || fournisseur.getNom().isEmpty()) {
+            throw new FournisseurException("nom is null");
+        }
+        if (fournisseur.getEmail() == null || fournisseur.getEmail().isEmpty()) {
+            throw new FournisseurException("email is null");
+        }
+        fournisseurRepository.save(fournisseur);
     }
 
     private void checkFournisseurIsNotNull(Fournisseur fournisseur) {
         if (fournisseur == null) {
-
+            throw new FournisseurException("fournisseur null");
         }
     }
+
 
 }
